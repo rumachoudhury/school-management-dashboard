951de22c0d15cb74c96fc985429dd1110b276e50
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-31 02:06:33 -0400

    Fix RootLayout and DashboardLayout structure again

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,32 +1,15 @@
-// export default function DashboardLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <div className="flex flex-col">
-//       {/* Sidebar placeholder */}
-//       <div className="">Dashboard</div>
-
-//       {/* Main content */}
-//       <div className="">{children}</div>
-//     </div>
-//   );
-// }
-
-import Navbar from "@/src/components/Navbar";
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    // <div className="flex">
-    <div className="flex-1 flex flex-col">
-      <Navbar />
-      <main className="flex items-center justify-center mt-4">{children}</main>
+    <div className="h-screen flex">
+      {/* Sidebar */}
+      <div className="w-64 bg-gray-900 text-white p-4">L</div>
+
+      {/* Content */}
+      <div className="flex-1 bg-gray-100 p-4">{children}</div>
     </div>
-    // </div>
   );
 }
