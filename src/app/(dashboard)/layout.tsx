d259861889fa80566c52c1a9d3e71e80cef6f065
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-01 00:30:22 -0400

    Fix navbar placement and improve dashboard layout structure

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Menu from "@/src/components/Menu";
 import Navbar from "@/src/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,21 +9,43 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
+    // <div className="h-screen flex">
+    //   {/* Sidebar */}
+    //   {/* <div className="w-64 bg-gray-900 text-white p-4  flex space-x-2">
+    //     <Link href="/">
+    //       <Image
+    //         src="/images/download.png"
+    //         alt="School Logo"
+    //         width={40}
+    //         height={40}
+    //         className="rounded-full"
+    //       />
+    //     </Link>
+    //     <span className="hidden lg:block font-semibold ">School</span>
+    //   </div> */}
+
+    //   <div className="w-64 bg-gray-900 text-white p-4">
+    //     <Link href="/" className="flex items-center justify-center gap-2 mb-8">
+    //       <Image
+    //         src="/images/download.png"
+    //         alt="School Logo"
+    //         width={40}
+    //         height={40}
+    //         className="rounded-full"
+    //       />
+    //       <span className="hidden lg:block font-semibold">School</span>
+    //     </Link>
+
+    //     <Menu />
+    //   </div>
+
+    //   {/* Content */}
+    //   <div className="flex-1 bg-gray-100 p-4">{children}</div>
+    // </div>
+
+    // ===================================
     <div className="h-screen flex">
       {/* Sidebar */}
-      {/* <div className="w-64 bg-gray-900 text-white p-4  flex space-x-2">
-        <Link href="/">
-          <Image
-            src="/images/download.png"
-            alt="School Logo"
-            width={40}
-            height={40}
-            className="rounded-full"
-          />
-        </Link>
-        <span className="hidden lg:block font-semibold ">School</span>
-      </div> */}
-
       <div className="w-64 bg-gray-900 text-white p-4">
         <Link href="/" className="flex items-center gap-2 mb-8">
           <Image
@@ -35,13 +58,15 @@ export default function DashboardLayout({
           <span className="hidden lg:block font-semibold">School</span>
         </Link>
 
-        {/* Navigation goes here later */}
-
-        <Navbar />
+        {/* Sidebar menu here */}
+        <Menu />
       </div>
 
-      {/* Content */}
-      <div className="flex-1 bg-gray-100 p-4">{children}</div>
+      {/* Right side */}
+      <div className="flex-1 bg-gray-100">
+        <Navbar />
+        <div className="p-4">{children}</div>
+      </div>
     </div>
   );
 }
