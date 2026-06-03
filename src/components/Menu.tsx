7979c0ae6f5baeaf456cf57e13f3d2fb2eea24e9
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-03 00:12:39 -0400

    Updated Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -125,9 +125,13 @@ const Menu = () => {
             {i.title}
           </span>
           {i.items.map((item) => (
-            <Link href={item.href} key={item.label}>
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-2 p-2 rounded-md hover:bg-gray-100"
+            >
               <Image src={item.icon} alt="" width={20} height={20} />
-              <span>{item.label}</span>
+              <span className="hidden lg:block">{item.label}</span>
             </Link>
           ))}
         </div>
