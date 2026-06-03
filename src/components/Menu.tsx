1622c0866f9bff54f281700315ebba99da87d59f
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-02 20:10:04 -0400

    updated the Menu component with some tailwind class

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -121,7 +121,9 @@ const Menu = () => {
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
-          <span>{i.title}</span>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
           {i.items.map((item) => (
             <Link href={item.href} key={item.label}>
               <Image src={item.icon} alt="" width={20} height={20} />
