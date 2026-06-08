ab3f786aed255a4c3786ba125784b9559054d955
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-08 00:27:14 -0400

    Fixed the UserCart Component

--- src/components/UserCart.tsx
+++ src/components/UserCart.tsx
@@ -1,7 +1,21 @@
+// import React from "react";
+
+// function UserCart({ type }: { type: string }) {
+//   return (
+//     <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4">UserCart</div>
+//   );
+// }
+
+// export default UserCart;
+
 import React from "react";
 
 function UserCart({ type }: { type: string }) {
-  return <div className="rounded-2xl odd:bg-purple">UserCart</div>;
+  return (
+    <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4">
+      UserCart - {type}
+    </div>
+  );
 }
 
 export default UserCart;
