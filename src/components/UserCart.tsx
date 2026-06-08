053d9d5a453ae2e60b8fba1cbf7c533cb5eb7423
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-08 01:00:08 -0400

    Update UserCart with styles and next image

--- src/components/UserCart.tsx
+++ src/components/UserCart.tsx
@@ -7,13 +7,23 @@
 // }
 
 // export default UserCart;
+// ==========================================
 
 import React from "react";
+import Image from "next/image";
 
 function UserCart({ type }: { type: string }) {
   return (
-    <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4">
-      UserCart - {type}
+    <div className="rounded-2xl odd:bg-[#7B61FF] even:bg-[#F9E5C9] p-4 flex-1">
+      <div>
+        <span>2025/26</span>
+        <Image
+          src="/images/school-management-dashboard/more.png"
+          alt="More image"
+          width={20}
+          height={20}
+        />
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,10 @@
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic":
-//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//       colors: {
-//         sky: "#C3EBFA",
-//         skyLight: "#EDF9FD",
-//         purple: "#7B61FF",
-//         purpleLight: "#F3E9FF",
-//         yellow: "#F9E5C9",
-//         yellowLight: "#FFF5E8",
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
-// export default config;
-
-// ======================================================
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -53,7 +23,33 @@ const config: Config = {
       },
     },
   },
+
   plugins: [],
 };
-
 export default config;
+
+// ======================================================
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         sky: "#C3EBFA",
+//         skyLight: "#EDF9FD",
+//         purple: "#7B61FF",
+//         purpleLight: "#F3E9FF",
+//         yellow: "#F9E5C9",
+//         yellowLight: "#FFF5E8",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+// export default config;
