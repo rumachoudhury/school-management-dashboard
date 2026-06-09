d206f02b0708c61fc2bfd539b843c08c997c4bc8
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-09 00:13:10 -0400

    Create CountChart component

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -1,4 +1,61 @@
-import React from "react";
+"use client";
+
+import { RadialBarChart, RadialBar, Legend, Tooltip } from "recharts";
+import { RechartsDevtools } from "@recharts/devtools";
+
+// #region Sample data
+const data = [
+  {
+    name: "18-24",
+    uv: 31.47,
+    pv: 2400,
+    fill: "#8884d8",
+  },
+  {
+    name: "25-29",
+    uv: 26.69,
+    pv: 4567,
+    fill: "#83a6ed",
+  },
+  {
+    name: "30-34",
+    uv: 15.69,
+    pv: 1398,
+    fill: "#8dd1e1",
+  },
+  {
+    name: "35-39",
+    uv: 8.22,
+    pv: 9800,
+    fill: "#82ca9d",
+  },
+  {
+    name: "40-49",
+    uv: 8.63,
+    pv: 3908,
+    fill: "#a4de6c",
+  },
+  {
+    name: "50+",
+    uv: 2.63,
+    pv: 4800,
+    fill: "#d0ed57",
+  },
+  {
+    name: "unknown",
+    uv: 6.67,
+    pv: 4800,
+    fill: "#ffc658",
+  },
+];
+
+// #endregion
+const style = {
+  top: "50%",
+  right: 0,
+  transform: "translate(0, -50%)",
+  lineHeight: "24px",
+};
 
 function CountChart() {
   return <div>CountChart</div>;
