4a65c1948102d9323447d6b4f342853465b4bb3c
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-09 00:01:21 -0400

    Added Recharts for dashboard charts

---
+++ src/components/CountChart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CountChart() {
+  return <div>CountChart</div>;
+}
+
+export default CountChart;
--- src/components/UserCart.tsx
+++ src/components/UserCart.tsx
@@ -1,30 +1,55 @@
 // import React from "react";
+// import Image from "next/image";
 
 // function UserCart({ type }: { type: string }) {
 //   return (
-//     <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4">UserCart</div>
+//     <div className="rounded-2xl odd:bg-[#7B61FF] even:bg-[#F9E5C9] p-4 flex-1 min-w-[170px]">
+//       <div className="flex flex-col justify-between items-center gap-2">
+//         <span className="text-xs bg-white px-2 py-1 rounded-full text-green-400">
+//           2025/26
+//         </span>
+//         <Image
+//           src="/images/school-management-dashboard/more.png"
+//           alt="More image"
+//           width={20}
+//           height={20}
+//         />
+//         <h1 className="text-2xl">1,234</h1>
+//         <span>{type}</span>
+//       </div>
+//     </div>
 //   );
 // }
 
 // export default UserCart;
-// ==========================================
-
 import React from "react";
 import Image from "next/image";
 
 function UserCart({ type }: { type: string }) {
   return (
-    <div className="rounded-2xl odd:bg-[#7B61FF] even:bg-[#F9E5C9] p-4 flex-1 min-w-[1300px]">
-      <div className="flex justify-between items-center">
-        <span>2025/26</span>
+    <div className="rounded-2xl p-4 flex-1 min-w-[170px] odd:bg-[#7B61FF] even:bg-[#F9E5C9]  shadow-md">
+      {/* top row */}
+      <div className="flex items-center justify-between">
+        <span className="text-xs bg-white/40 px-2 py-1 rounded-full text-green-700">
+          2025/26
+        </span>
+
         <Image
           src="/images/school-management-dashboard/more.png"
           alt="More image"
-          width={20}
-          height={20}
+          width={18}
+          height={18}
         />
-        <h1>1,234</h1>
-        <span>{type}</span>
+      </div>
+
+      {/* middle number */}
+      <div className="mt-4">
+        <h1 className="text-2xl font-bold">1,234</h1>
+      </div>
+
+      {/* bottom label */}
+      <div className="mt-2">
+        <span className="capitalize text-sm opacity-80">{type}</span>
       </div>
     </div>
   );
