2fb85b35064ae0ac446982c2804da1da54aeb608
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-11 16:38:24 -0400

    Add sample attendance data

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -2,6 +2,34 @@
 
 import React from "react";
 
+const data = [
+  {
+    name: " Monday",
+    present: 4000,
+    absent: 2400,
+  },
+  {
+    name: "Twesday",
+    present: 4000,
+    absent: 2400,
+  },
+  {
+    name: "Wednesday",
+    present: 4000,
+    absent: 2400,
+  },
+  {
+    name: "Thrusday",
+    present: 4000,
+    absent: 2400,
+  },
+  {
+    name: "Friday",
+    present: 4000,
+    absent: 2400,
+  },
+];
+
 function AttendanceChart() {
   return <div></div>;
 }
