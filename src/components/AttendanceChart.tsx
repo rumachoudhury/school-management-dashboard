6e71c14e702a7b792c15bc5c968cc9d87e424848
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-11 16:44:24 -0400

    Add Recharts SimpleBarChart component

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+import { RechartsDevtools } from "@recharts/devtools";
 
 const data = [
   {
@@ -31,7 +43,40 @@ const data = [
 ];
 
 function AttendanceChart() {
-  return <div></div>;
+  return (
+    <div style={{ width: "100%", height: 400 }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={data}
+          margin={{ top: 5, right: 10, left: 10, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" opacity={0.4} />
+
+          <XAxis dataKey="name" />
+          <YAxis />
+
+          <Tooltip />
+          <Legend />
+
+          <Bar
+            dataKey="pv"
+            fill="#8884d8"
+            radius={[10, 10, 0, 0]}
+            activeBar={{ fill: "pink", stroke: "blue" }}
+          />
+
+          <Bar
+            dataKey="uv"
+            fill="#82ca9d"
+            radius={[10, 10, 0, 0]}
+            activeBar={{ fill: "gold", stroke: "purple" }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+
+      {process.env.NODE_ENV === "development" && <RechartsDevtools />}
+    </div>
+  );
 }
 
 export default AttendanceChart;
