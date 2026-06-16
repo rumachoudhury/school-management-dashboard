2f46820084dd83510fcdbd9ddbe63ead02bbcfc0
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-15 23:26:47 -0400

    Import react-calendar styles

--- src/components/EventCalender.tsx
+++ src/components/EventCalender.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-
+import "react-calendar/dist/Calendar.css";
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -10,7 +10,7 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 function EventCalender() {
   const [value, onChange] = useState<Value>(new Date());
   return (
-    <div>
+    <div className="bg-white p-4 rounded-md">
       <Calendar onChange={onChange} value={value} />
     </div>
   );
