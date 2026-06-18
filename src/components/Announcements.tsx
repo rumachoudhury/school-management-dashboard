7e88c7cfd0dabba8587e05f3e954bb1d676bd062
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-18 14:11:13 -0400

    Add some style to the Announcements component

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -4,8 +4,8 @@ const Announcements = () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <div className="flex items-center justify-between">
-        <h1>Announcements</h1>
-        <span>View All</span>
+        <h1 className="text-xl font-semibold">Announcements</h1>
+        <span className="text-xs">View All</span>
       </div>
     </div>
   );
