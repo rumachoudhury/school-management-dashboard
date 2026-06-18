0546d013ae29752bc9aee3096fd0b59de31e59a6
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-18 14:08:54 -0400

    Updated the Announcements component

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Announcements = () => {
-  return <div></div>;
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1>Announcements</h1>
+        <span>View All</span>
+      </div>
+    </div>
+  );
 };
 
 export default Announcements;
