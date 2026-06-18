922083a7b8ff152bea04840e3e038fb4bdfe15cd
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-18 14:17:46 -0400

    Add another section to the Announcements component

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -7,6 +7,14 @@ const Announcements = () => {
         <h1 className="text-xl font-semibold">Announcements</h1>
         <span className="text-xs">View All</span>
       </div>
+
+      <div className="flex flex-col gap-4">
+        <div className="bg-sky-200 rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-semibold">New School Announcement</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
