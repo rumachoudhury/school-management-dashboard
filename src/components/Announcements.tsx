f9ac15c5f76a85c463c8a9bcd866db2c203ddb91
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-20 13:38:14 -0400

    Updated Announcements component

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -7,37 +7,8 @@ const Announcements = () => {
         <h1 className="text-xl font-semibold">Announcements</h1>
         <span className="text-xs">View All</span>
       </div>
-      {/* 
-      <div className="flex flex-col gap-4">
-        <div className="bg-purple-200 rounded-md p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="font-semibold">New School Announcement</h2>
-            <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">
-              06-19-2026
-            </span>
 
-            <p className="text-sm text-gray-600 mt-2">
-              Discuss student progress and upcoming activities.
-            </p>
-          </div>
-        </div>
-
-        <div className="bg-yellow-200 rounded-md p-4">
-          <div className="flex items-center justify-between">
-            <h2 className="font-semibold">Parent Meeting Reminder</h2>
-            <span className="text-sm text-gray-400 bg-white rounded-md px-1 py-1">
-              06-19-2026
-            </span>
-
-            <p className="text-sm text-gray-600 mt-2">
-              Parents are invited to discuss student progress and upcoming
-              activities.
-            </p>
-          </div>
-        </div>
-      </div> */}
-
-      {/* School Announcement card */}
+      {/* announcement cards */}
       <div className="flex flex-col gap-4">
         <div className="bg-purple-200 rounded-md p-4">
           <div className="flex items-center justify-between">
