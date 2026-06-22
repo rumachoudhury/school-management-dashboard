397886e2754fdf208a6f3c6cbf1ef3189659a193
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-22 08:03:08 -0400

    Fixed react-big-calendar Next.js compatibility issue

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,6 +1,31 @@
+// "use client";
+
+// import { Calendar, momentLocalizer } from "react-big-calendar";
+// import moment from "moment";
+// import { calendarEvents } from "../lib/data";
+
+// const localizer = momentLocalizer(moment);
+
+// const BigCalendar = () => (
+//   <div>
+//     <Calendar
+//       localizer={localizer}
+//       events={calendarEvents}
+//       startAccessor="start"
+//       endAccessor="end"
+//       style={{ height: 500 }}
+//     />
+//   </div>
+// );
+
+// export default BigCalendar;
+// ======================================
+"use client";
+
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import { calendarEvents } from "../lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
