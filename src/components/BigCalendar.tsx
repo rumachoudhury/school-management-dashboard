125dce3ba93e1384f71cb6fdf5d6fa1b2318ef02
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-22 07:52:12 -0400

    Created BigCalendar component using react-big-calendar

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,16 +1,19 @@
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import { calendarEvents } from "../lib/data";
 
 const localizer = momentLocalizer(moment);
 
-const MyCalendar = (props) => (
+const BigCalendar = () => (
   <div>
     <Calendar
       localizer={localizer}
-      events={myEventsList}
+      events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
     />
   </div>
 );
+
+export default BigCalendar;
