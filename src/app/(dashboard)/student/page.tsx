7789d47437b1dc9f43565af613b839f7879fa7c9
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-22 07:29:41 -0400

    Updated the student page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -6,7 +6,9 @@ function StudentPage() {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* Left */}
-      <div className="w-ful xl:w-2/3"></div>
+      <div className="w-ful xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">Schedule (4A)</div>
+      </div>
 
       {/* Right */}
       <div className="w-full xl:w-1/3">
