59b1ac3df123eb590fab81b629d65764d7212abc
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-21 22:37:15 -0400

    Created student page add EventCalendar and Announcements components

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,22 @@
+import Announcements from "@/src/components/Announcements";
+import EventCalendar from "@/src/components/EventCalendar";
 import React from "react";
 
 function StudentPage() {
-  return <div>StudentPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* Left */}
+      <div className="w-ful xl:w-2/3"></div>
+
+      {/* Right */}
+      <div className="w-full xl:w-1/3">
+        <div className="w-full xl:w-1/3 flex flex-col gap-8">
+          <EventCalendar />
+          <Announcements />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StudentPage;
