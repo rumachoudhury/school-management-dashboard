87427bb8dbed6f440300ec5e99ac0e6d043be4af
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-24 18:44:33 -0400

    Updated the BigCalenda component

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,32 +1,3 @@
-// import Announcements from "@/src/components/Announcements";
-// import BigCalendar from "@/src/components/BigCalendar";
-// import EventCalendar from "@/src/components/EventCalendar";
-// import React from "react";
-
-// function StudentPage() {
-//   return (
-//     <div className="p-4 flex gap-4 flex-col xl:flex-row">
-//       {/* Left */}
-//       <div className="w-ful xl:w-2/3">
-//         <div className="h-full bg-white p-4 rounded-md">
-//           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-//           <BigCalendar />
-//         </div>
-//       </div>
-
-//       {/* Right */}
-//       <div className="w-full xl:w-1/3">
-//         <div className="w-full xl:w-1/3 flex flex-col gap-8">
-//           <EventCalendar />
-//           <Announcements />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default StudentPage;
-// ============================
 import Announcements from "@/src/components/Announcements";
 import BigCalendar from "@/src/components/BigCalendar";
 import EventCalendar from "@/src/components/EventCalendar";
--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,25 +1,3 @@
-// "use client";
-
-// import { Calendar, momentLocalizer } from "react-big-calendar";
-// import moment from "moment";
-// import { calendarEvents } from "../lib/data";
-
-// const localizer = momentLocalizer(moment);
-
-// const BigCalendar = () => (
-//   <div>
-//     <Calendar
-//       localizer={localizer}
-//       events={calendarEvents}
-//       startAccessor="start"
-//       endAccessor="end"
-//       style={{ height: 500 }}
-//     />
-//   </div>
-// );
-
-// export default BigCalendar;
-// ======================================
 "use client";
 
 import { Calendar, momentLocalizer } from "react-big-calendar";
