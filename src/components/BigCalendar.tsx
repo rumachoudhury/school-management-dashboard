b77eaa2fc93020c9a2eb25a770bd48ca8110462f
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-24 18:58:22 -0400

    Set default calendar view to work week

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -3,7 +3,7 @@
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import { calendarEvents } from "../lib/data";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/css/react-big-calendar.css"; //without this, the calendar will not be styled correctly
 
 const localizer = momentLocalizer(moment);
 
@@ -14,6 +14,8 @@ const BigCalendar = () => (
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
+      views={["work_week", "day", "agenda"]}
+      view="work_week"
       style={{ height: 500 }}
     />
   </div>
