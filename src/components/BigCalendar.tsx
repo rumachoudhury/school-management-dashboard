ef63924ca81b62b0f542ccbbecd56f39e2b49154
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-24 19:09:23 -0400

    Add calendar view state and set work week default view

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import { calendarEvents } from "../lib/data";
 import "react-big-calendar/lib/css/react-big-calendar.css"; //without this, the calendar will not be styled correctly
+import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
-const BigCalendar = () => (
-  <div>
+const BigCalendar = () => {
+  const [view, setView] = useState<view>(Views.WORK_WEEK);
+  return (
     <Calendar
       localizer={localizer}
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
       views={["work_week", "day", "agenda"]}
-      view="work_week"
+      view={view}
+      onView={(newView) => setView(newView)}
       style={{ height: 500 }}
     />
-  </div>
-);
+  );
+};
 
 export default BigCalendar;
