2c08a41d7c3a054fbb1a3c679ca76891f13b47cf
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-24 19:53:10 -0400

    Fix calendar view type import

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import {
+  Calendar,
+  momentLocalizer,
+  Views,
+  type View,
+} from "react-big-calendar";
 import moment from "moment";
 import { calendarEvents } from "../lib/data";
-import "react-big-calendar/lib/css/react-big-calendar.css"; //without this, the calendar will not be styled correctly
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 
 const localizer = momentLocalizer(moment);
 
 const BigCalendar = () => {
-  const [view, setView] = useState<view>(Views.WORK_WEEK);
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
   return (
     <Calendar
       localizer={localizer}
@@ -19,6 +25,8 @@ const BigCalendar = () => {
       views={["work_week", "day", "agenda"]}
       view={view}
       onView={(newView) => setView(newView)}
+      min={new Date(2026, 0, 1, 8, 0)}
+      max={new Date(2026, 0, 1, 17, 0)}
       style={{ height: 500 }}
     />
   );
