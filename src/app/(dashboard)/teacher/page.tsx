279709a23e25e1439ac7507ddbcfede5e4383c22
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-26 13:20:12 -0400

    Fixed teacher schedule page layout

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -10,14 +10,14 @@ function TeacherPage() {
       {/* Left */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <h1 className="text-xl font-semibold">Schedule</h1>
           <BigCalendar />
         </div>
       </div>
 
       {/* Right */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        {/* <EventCalendar /> */}
         <Announcements />
       </div>
     </div>
