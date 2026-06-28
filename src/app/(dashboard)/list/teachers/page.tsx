8f14cc9c1e45be5e5a8049936db44969ea3d1423
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-27 23:40:44 -0400

    Add teacher list filter buttons and search layout

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,14 +1,32 @@
 import TableSearch from "@/src/components/TableSearch";
+import Image from "next/image";
 
 export default function TeacherList() {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* Top */}
-      <div className="">
-        <h1>All Teachers List</h1>
+      <div className="flex items-center justify-between">
+        <h2 className="text-lg font-semibold">All Teachers List</h2>
 
-        <div className="">
-          <TableSearch />
+        <div className="flex items-center gap-4">
+          <div>
+            <TableSearch />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100">
+              <Image
+                src="/images/school-management-dashboard/filter.png"
+                alt="Filter"
+                width={14}
+                height={14}
+              />
+            </button>
+
+            <button className="w-8 h-8 rounded-full bg-yellow-100"></button>
+
+            <button className="w-8 h-8 rounded-full bg-yellow-100"></button>
+          </div>
         </div>
       </div>
 
