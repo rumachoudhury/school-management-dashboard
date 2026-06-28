e48400915c211a8f42b35849c0a9ed0fc76713b5
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-27 23:14:32 -0400

    Remove event calendar from parent page

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Announcements from "@/src/components/Announcements";
 import BigCalendar from "@/src/components/BigCalendar";
-import EventCalendar from "@/src/components/EventCalendar";
+// import EventCalendar from "@/src/components/EventCalendar";
 
 function ParentPage() {
   return (
@@ -10,14 +10,14 @@ function ParentPage() {
       {/* Left */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule</h1>
+          <h1 className="text-xl font-semibold">Schedule (John Dee)</h1>
           <BigCalendar />
         </div>
       </div>
 
       {/* Right */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        {/* <EventCalendar /> */}
         <Announcements />
       </div>
     </div>
