534e1b7b88e039b9d12a930145330f3f8f883c6f
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-06-29 23:02:08 -0400

    Add page number buttons to pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -10,6 +10,14 @@ function Pagination() {
         Prev
       </button>
 
+      <div>
+        <button className="px-2 rounded-sm bg-sky-200">1</button>
+        <button className="px-2 rounded-sm">2</button>
+        <button className="px-2 rounded-sm">3</button>
+        ...
+        <button className="px-2 rounded-sm">10</button>
+      </div>
+
       <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
         Next
       </button>
