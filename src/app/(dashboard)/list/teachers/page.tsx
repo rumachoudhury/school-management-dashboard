bcb1a2b620e53c25dc9d9f147896db2a7f765894
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-01 19:09:20 -0400

    Add teacher table data and admin action button

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -4,6 +4,7 @@ import Table from "@/src/components/Table";
 import TableSearch from "@/src/components/TableSearch";
 import Image from "next/image";
 import Link from "next/link";
+import { role, teachersData } from "@/src/lib/data";
 
 type Column = {
   header: string;
@@ -13,29 +14,15 @@ type Column = {
 
 type Teacher = {
   id: number;
-  teacherID: string;
+  teacherId: string;
   name: string;
   email: string;
   photo: string;
   phone: string;
   subjects: string[];
   classes: string[];
-  adress: string;
+  address: string;
 };
-
-const teachers: Teacher[] = [
-  {
-    id: 1,
-    teacherID: "T001",
-    name: "John",
-    email: "john@test.com",
-    photo: "/avatar.png",
-    phone: "123456789",
-    subjects: ["Math"],
-    classes: ["5A"],
-    adress: "New York",
-  },
-];
 
 const columns: Column[] = [
   {
@@ -44,7 +31,7 @@ const columns: Column[] = [
   },
   {
     header: "Teacher ID",
-    accessor: "teacherID",
+    accessor: "teacherId",
     className: "hidden md:table-cell",
   },
   {
@@ -70,62 +57,17 @@ const columns: Column[] = [
 ];
 
 export default function TeacherListPage() {
-  // const renderRow = (item: Teacher) => {
-  //   return (
-  //     <tr key={item.id}>
-  //       <td>
-  //         <Image
-  //           src={item.photo}
-  //           alt=""
-  //           width={40}
-  //           height={40}
-  //           className="md:hidden xl:block w-10 h-11 rounded-full object-cover"
-  //         />
-
-  //         <div className="flex flex-col md:flex-row items-start md:items-center gap-2">
-  //           <h3 className="font-semibold">{item.name}</h3>
-  //           <p className="text-sm text-gray">{item.email}</p>
-  //         </div>
-  //       </td>
-
-  //       <td className="hidden md:table-cell">{item.teacherID}</td>
-  //       <td className="hidden md:table-cell">{item.subjects.join(", ")}</td>
-  //       <td className="hidden md:table-cell">{item.classes.join(", ")}</td>
-  //       <td className="hidden lg:table-cell">{item.phone}</td>
-  //       <td className="hidden lg:table-cell">{item.adress}</td>
-
-  //       <td>
-  //         <div className="flex items-center gap-2">
-  //           <Link
-  //             href={`/list/teachers/${item.id}`}
-  //             className="text-sm text-blue-500"
-  //           >
-  //             <button className="w-7 h-7 flex items-center justify-center rounded-full">
-  //               <Image
-  //                 src="/images/school-management-dashboard/view.png"
-  //                 alt=""
-  //                 width={16}
-  //                 height={16}
-  //               />
-  //             </button>
-  //           </Link>
-  //         </div>
-  //       </td>
-  //     </tr>
-  //   );
-  // };
-
   const renderRow = (item: Teacher) => {
     return (
-      <tr key={item.id}>
-        <td>
+      <tr key={item.id} className="">
+        <td className="flex items-center justify-between">
           <Link
             href={`/list/teachers/${item.id}`}
-            className="flex items-center gap-2"
+            className="flex items-center gap-4 py-2"
           >
             <Image
               src={item.photo}
-              alt={item.name}
+              alt=""
               width={40}
               height={40}
               className="w-10 h-10 rounded-full object-cover"
@@ -138,7 +80,7 @@ export default function TeacherListPage() {
           </Link>
         </td>
 
-        <td className="hidden md:table-cell">{item.teacherID}</td>
+        <td className="hidden md:table-cell">{item.teacherId}</td>
 
         <td className="hidden md:table-cell">{item.subjects.join(", ")}</td>
 
@@ -146,11 +88,11 @@ export default function TeacherListPage() {
 
         <td className="hidden lg:table-cell">{item.phone}</td>
 
-        <td className="hidden lg:table-cell">{item.adress}</td>
+        <td className="hidden lg:table-cell">{item.address}</td>
 
         <td>
           <Link href={`/list/teachers/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full">
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky-100">
               <Image
                 src="/images/school-management-dashboard/view.png"
                 alt="View"
@@ -159,6 +101,16 @@ export default function TeacherListPage() {
               />
             </button>
           </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple-100">
+              <Image
+                src="/images/school-management-dashboard/delete.png"
+                alt="View"
+                width={16}
+                height={16}
+              />
+            </button>
+          )}
         </td>
       </tr>
     );
@@ -210,7 +162,8 @@ export default function TeacherListPage() {
 
       {/* List */}
       <div className="">
-        <Table columns={columns} data={teachers} renderRow={renderRow} />
+        {/* <Table columns={columns} data={teachers} renderRow={renderRow} /> */}
+        <Table columns={columns} data={teachersData} renderRow={renderRow} />
       </div>
 
       {/* Pagination */}
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,37 @@
+// import React from "react";
+
+// type Column = {
+//   header: string;
+//   accessor: string;
+//   className?: string;
+// };
+// function Table({
+//   columns,
+//   data,
+//   renderRow,
+// }: {
+//   columns: Column[];
+//   data: any[];
+//   renderRow: (item: any) => React.ReactNode;
+// }) {
+//   return (
+//     <table className="w-full mt-4">
+//       <thead>
+//         <tr className="text-left text-gray-500 text-sm">
+//           {columns.map((column) => (
+//             <th key={column.accessor} className={column.className}>
+//               {column.header}
+//             </th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>{data.map((item) => renderRow(item))}</tbody>
+//     </table>
+//   );
+// }
+
+// export default Table;
+// ==========================
 import React from "react";
 
 type Column = {
@@ -5,14 +39,15 @@ type Column = {
   accessor: string;
   className?: string;
 };
-function Table({
+
+function Table<T>({
   columns,
   data,
   renderRow,
 }: {
   columns: Column[];
-  data: any[];
-  renderRow: (item: any) => React.ReactNode;
+  data: T[];
+  renderRow: (item: T) => React.ReactNode;
 }) {
   return (
     <table className="w-full mt-4">
@@ -25,6 +60,7 @@ function Table({
           ))}
         </tr>
       </thead>
+
       <tbody>{data.map((item) => renderRow(item))}</tbody>
     </table>
   );
