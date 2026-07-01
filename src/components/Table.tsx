49b792461ea63efcd4a5714c64feaa9b545ff596
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-01 19:11:47 -0400

    Update the Table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,37 +1,3 @@
-// import React from "react";
-
-// type Column = {
-//   header: string;
-//   accessor: string;
-//   className?: string;
-// };
-// function Table({
-//   columns,
-//   data,
-//   renderRow,
-// }: {
-//   columns: Column[];
-//   data: any[];
-//   renderRow: (item: any) => React.ReactNode;
-// }) {
-//   return (
-//     <table className="w-full mt-4">
-//       <thead>
-//         <tr className="text-left text-gray-500 text-sm">
-//           {columns.map((column) => (
-//             <th key={column.accessor} className={column.className}>
-//               {column.header}
-//             </th>
-//           ))}
-//         </tr>
-//       </thead>
-//       <tbody>{data.map((item) => renderRow(item))}</tbody>
-//     </table>
-//   );
-// }
-
-// export default Table;
-// ==========================
 import React from "react";
 
 type Column = {
