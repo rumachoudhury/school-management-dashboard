12df8374d4174446afca99c15c569ac96eaa1f3e
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-07 10:17:42 -0400

    Created Assignment list page

---
+++ src/app/(dashboard)/list/assignment/page.tsx
@@ -0,0 +1,145 @@
+import Pagination from "@/src/components/Pagination";
+import Table from "@/src/components/Table";
+
+import TableSearch from "@/src/components/TableSearch";
+import Image from "next/image";
+import Link from "next/link";
+import { role, examsData } from "@/src/lib/data";
+
+type Column = {
+  header: string;
+  accessor: string;
+  className?: string;
+};
+
+type Assignment = {
+  id: number;
+  subject: string;
+  class: string;
+  teacher: string;
+  date: string;
+};
+
+const columns: Column[] = [
+  {
+    header: "Subject Names",
+    accessor: "subject",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+    className: "text-right",
+  },
+];
+export default function AssignmentListPage() {
+  const renderRow = (item: Assignment) => {
+    return (
+      <tr
+        key={item.id}
+        className="border-b border-gray-200 text-sm hover:bg-gray-50"
+      >
+        <td className="p-4">{item.subject}</td>
+
+        <td className="p-4">{item.class}</td>
+
+        <td className="hidden md:table-cell">{item.teacher}</td>
+
+        <td>
+          <div className="flex items-center gap-2 justify-end">
+            <Link href={`/list/lessons/${item.id}`}>
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky-100">
+                <Image
+                  src="/images/school-management-dashboard/edit.png"
+                  alt="View"
+                  width={16}
+                  height={16}
+                />
+              </button>
+            </Link>
+
+            {role === "admin" && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple-100">
+                <Image
+                  src="/images/school-management-dashboard/delete.png"
+                  alt="Delete"
+                  width={16}
+                  height={16}
+                />
+              </button>
+            )}
+          </div>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h2 className="hidden md:block text-lg font-semibold">All Exam List</h2>
+
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <div className="w-64">
+            <TableSearch />
+          </div>
+
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100">
+              <Image
+                src="/images/school-management-dashboard/filter.png"
+                alt="Filter"
+                width={14}
+                height={14}
+              />
+            </button>
+
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100">
+              <Image
+                src="/images/school-management-dashboard/sort.png"
+                alt="Filter"
+                width={14}
+                height={14}
+              />
+            </button>
+
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-100">
+                <Image
+                  src="/images/school-management-dashboard/plus.png"
+                  alt="Filter"
+                  width={14}
+                  height={14}
+                />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* List */}
+      <div className="">
+        <Table columns={columns} data={examsData} renderRow={renderRow} />
+      </div>
+
+      {/* Pagination */}
+      <div className="">
+        <Pagination />
+      </div>
+    </div>
+  );
+}
