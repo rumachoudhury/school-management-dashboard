70d03b76cca85e19a29a9bfb397e02f2deee40ba
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-13 23:50:24 -0400

    add some section to the SingleTeacherPage

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -4,7 +4,19 @@ function SingleTeacherPage() {
   return (
     <div className="flex-1 p-4 flex flex-col xl:flex-row">
       {/* Left */}
-      <div className="w-full xl:w-2/3">L</div>
+      <div className="w-full xl:w-2/3">
+        {/* Top */}
+        <div className="">
+          {/* User info card  */}
+          <div className=""></div>
+
+          {/* Small card with some info */}
+          <div className=""></div>
+        </div>
+
+        {/* Bottom */}
+        <div className="">Schedule</div>
+      </div>
 
       {/* Right */}
       <div className="w-full xl:w-1/3">R</div>
