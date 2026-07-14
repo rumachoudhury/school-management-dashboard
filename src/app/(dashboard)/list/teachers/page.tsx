dc91e24ff42f4041e4e18be404e6634fbd5c485c
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-13 23:32:56 -0400

    Add some styles to the SingleTeacherPage

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 function SingleTeacherPage() {
   return (
     <div className="flex-1 p-4 flex flex-col xl:flex-row">
-      SingleTeacherPage
+      {/* Left */}
+      <div className="w-full xl:w-2/3"></div>
+
+      {/* Right */}
+      <div className="w-full xl:w-2/3"></div>
     </div>
   );
 }
