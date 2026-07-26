47d213beb66f4f037422c311a60596a036e50900
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-07-25 21:25:54 -0400

    Configure Next.js Image component for Pexels images

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 function SingleTeacherPage() {
   return (
@@ -8,10 +9,21 @@ function SingleTeacherPage() {
         {/* Top */}
         <div className="flex flex-col lg:flex-row gap-4">
           {/* User info card  */}
-          <div className="bg-sky-400 py-6 px-4 rounded-md"></div>
+          <div className="bg-sky-400 py-6 px-4 rounded-md flex flex-1 gap-4">
+            <div className="w-1/3">
+              <Image
+                src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 rounded-full object-cover"
+              />
+            </div>
+            <div className="w-2/3"></div>
+          </div>
 
           {/* Small card with some info */}
-          <div className=""></div>
+          <div className="flex flex-1"></div>
         </div>
 
         {/* Bottom */}
