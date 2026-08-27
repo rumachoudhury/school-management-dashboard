72a56cad85db90942d43bc4c1c1c67405ef7017e
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-08-27 08:21:38 -0400

    Add performance chart

--- src/components/Performance.tsx
+++ src/components/Performance.tsx
@@ -8,16 +8,16 @@ export default function Performance() {
 
 // #region Sample data
 const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 },
-  { name: 'Group F', value: 189 },
+{ name: "Performance", value: 92, fill: "#60a5fa" }, 
+{ name: "Remaining", value: 8, fill: "#fde68a" },
+//   { name: 'Group C', value: 300 },
+//   { name: 'Group D', value: 200 },
+//   { name: 'Group E', value: 278 },
+//   { name: 'Group F', value: 189 },
 ];
 
 
     return(
-        <div></div>
+        <div>Performance</div>
     )
 }
